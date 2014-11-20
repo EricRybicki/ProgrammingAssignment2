65e7b6514248763f90e39123c389a663a9e93637
EricRybicki <e.rybicki12@gmail.com>
2014-11-20 16:32:02 +0100

    ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,19 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## makeCacheMatrix takes data in the form of a matrix and creates a list object
+## that can cache its inverse.
+makeCacheMatrix <- function(x = matrix()) {      # input x as a matrix
+    m <- NULL                                    # inverse variable resets per function call
+    set <- function(y) {                         # Function to clear cache
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() {x}                        # assigns original value of matrix to 'get'
+    setinverse <- function(solve) {m <<- solve}  # cacheSolve to assigns inverse into
+                                                 # cache using superassignment
+    getinverse <- function() {m}                 # accesed by cacheSolve to return
+                                                 # cached inverse, or NULL
+    list(set = set, get = get,                   # list with internal value defined functions
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
